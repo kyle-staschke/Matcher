b3026805b4c34b2e877709d906c0e8298709dc70
Julian Burner <burnerjulian@gmail.com>
2022-11-20 22:14:43 +0100

    Handle methods with inherited mapped names correctly

--- src/matcher/type/ClassInstance.java
+++ src/matcher/type/ClassInstance.java
@@ -920,7 +920,7 @@ public final class ClassInstance implements ParentEntry, Matchable<ClassInstance
 	@Override
 	public boolean hasMappedChildren() {
 		for (MethodInstance mth : methods) {
-			if (mth.hasMappedName() || mth.hasMappedChildren()) return true;
+			if (mth.hasNonInheritedMappedName() || mth.hasMappedChildren()) return true;
 		}
 
 		for (FieldInstance fld : fields) {
